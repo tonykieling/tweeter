0590d4f65f82c2c4398b1891bdac06e832867a30
Tony Kieling <tony.kieling@gmail.com>
2019-01-23 04:54:07 +0000

    missing flags and css file organized

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,30 @@
+$(function () {
+
+  const maxCharacteres = 140;
+
+  let textareaElement = $("#txtarea");
+  $(textareaElement).on("input", function() {
+    
+    let length = $(this).val().length;
+    length = maxCharacteres - length;
+    $("#counter").html(length);
+    
+    if (length < 0){
+      $("#counter").css({color: "red"});
+    } else {
+      $("#counter").css({color: "black"});
+    }
+  });
+
+  // let tweeter = $("#tweet-container");
+  $("#tweet-container").hover(function(){
+    $(this).css("border", ("solid 2px grey"));
+    $(this).css("fontWeight", "bold");
+    $(this).css("opacity", 1);}
+    , function(){
+      $(this).css("border silver 1px", );
+      $(this).css("fontWeight", "normal");
+      $(this).css("opacity", 0.8);
+      });
+
+});
