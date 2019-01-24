bc8da283a068bb973b1c219568eb7a65de2d92cf
Tony Kieling <tony.kieling@gmail.com>
2019-01-24 23:04:37 +0000

    fetching and refetch are done

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,55 +1,3 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
-
-// const data = [
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-//         "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-//         "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-//       },
-//       "handle": "@SirIsaac"
-//     },
-//     "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants asd asd asd asd asd asd asd asd asd "
-//     },
-//     "created_at": 1461116232227
-//   },
-//   {
-//     "user": {
-//       "name": "Descartes",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
-//         "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
-//         "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
-//       },
-//       "handle": "@rd" },
-//     "content": {
-//       "text": "Je pense , donc je suis"
-//     },
-//     "created_at": 1461113959088
-//   },
-//   {
-//     "user": {
-//       "name": "Johann von Goethe",
-//       "avatars": {
-//         "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
-//         "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
-//         "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
-//       },
-//       "handle": "@johann49"
-//     },
-//     "content": {
-//       "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
-//     },
-//     "created_at": 1461813796368
-//   }
-// ];
 
 function createTweetElement(tweetData){
   const days = (Math.round((((Date.now()) - (Number(tweetData.created_at))) / 1000) / 86400));
@@ -83,43 +31,32 @@ function createTweetElement(tweetData){
 let tweetContainer = $('#all-tweets');
 
 function renderTweets(tweets) {
-  console.log("renderTweets and tweets: ");
-  // tweetContainer.empty();
   for (let i = 0; i < tweets.length; i += 1){
     const tweet = createTweetElement(tweets[i]);
-    // console.log(`$tweet[${i}]: , ${tweet}`);$("tweets-container")
-    console.log(tweet);
     tweetContainer.prepend(tweet);
   };
 }
 
-// renderTweets(data);
-
 
 function loadTweets() {
-  console.log("getAlltweets::");
   $.ajax({
     method: "GET",
     url: "/tweets",
     dataType: 'json'
   }).done(function(data) {
-      console.log("data:: ", data)
+      console.log("here");
+      $('#all-tweets').empty();
       renderTweets(data);     
-      // allTweets.prepend(element);
   });
 }
 
-// request to server
-// let asd = request...?
 loadTweets();
 
 // post request from user
 $("#tweets-maker").on('submit', function(event) {
-  console.log("hit the button, event: ", event);
   // prevent the default behavor
   event.preventDefault();
   // get the data from the form
-  // const content = $(this).find('input').val();
   // ajax post request
   const serialized = $(this).serialize();
   console.log("serialized: ", serialized);
@@ -130,11 +67,8 @@ $("#tweets-maker").on('submit', function(event) {
     url: "/tweets",
     data: serialized
   }).done(function() {
-    console.log("AJAX #tweeter-maker", serialized);
     form.reset();
     // on success, refresh the creaks on the page    
     loadTweets(serialized);
-    // renderTweets(serialized);
-    // getAllCreaks();
   });
 });
