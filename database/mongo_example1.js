08a2b214d99f3a829d8917d8c881cc6eeef7502a
Tony Kieling <tony.kieling@gmail.com>
2019-01-26 02:45:10 +0000

    message length validation ok

---
+++ database/mongo_example1.js
@@ -0,0 +1,93 @@
+"use strict";
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to mongodb: ${MONGODB_URI}`);
+
+  // remove all document from tweets
+  db.tweets.remove({});
+
+  var tweets = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+  db.tweets.insert(tweets);
+
+
+  // ==> Refactored and wrapped as new, tweet-specific function:
+
+  function getTweets(callback) {
+    // db.collection("tweets").find().toArray(callback);
+    // db.tweets.find().toArray(callback);
+    db.collection("tweets").find({}, {"_id":0}).toArray(callback);
+
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    // console.log("tweets: ", tweets);
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+  });
+
+});
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,76 +1,91 @@
 
-function createTweetElement(tweetData){
-  const days = (Math.round((((Date.now()) - (Number(tweetData.created_at))) / 1000) / 86400));
-  const $tweet = `
-  <article class="tweet-container">
+$(function (){
+  function createTweetElement(tweetData){
+    const days = (Math.round((((Date.now()) - (Number(tweetData.created_at))) / 1000) / 86400));
+    const $tweet = `
+    <article class="tweet-container">
 
-        <header class="header">
-          <img src=${tweetData.user.avatars.small} name="avatar">
-          <p class="namePerson" name="namePerson">${tweetData.user.name}</p>
-          <p class="idPerson" name="idPerson">${tweetData.user.handle}</p>          
-        </header>
-        
-        <p class="message" name="message">${tweetData.content.text}</p>
-        
-        <footer class="footer">
-          <p name="timeAgo">${days} Days Ago
+          <header class="header">
+            <img src=${tweetData.user.avatars.small} name="avatar">
+            <p class="namePerson" name="namePerson">${tweetData.user.name}</p>
+            <p class="idPerson" name="idPerson">${tweetData.user.handle}</p>          
+          </header>
+          
+          <p class="message" name="message">${tweetData.content.text}</p>
+          
+          <footer class="footer">
+            <p name="timeAgo">${days} Days Ago
 
-          </p>
-          <div class="icons">
-            <img name="icon1" src="images/flag.png">
-            <img name="icon2" src="images/retweet.png">
-            <img name="icon3" src="images/heart.png">
-          </div>
-        </footer>
+            </p>
+            <div class="icons">
+              <img name="icon1" src="images/flag.png">
+              <img name="icon2" src="images/retweet.png">
+              <img name="icon3" src="images/heart.png">
+            </div>
+          </footer>
 
-      </article>
-  `;
-  return($tweet);  
-}
-
-
-let tweetContainer = $('#all-tweets');
-
-function renderTweets(tweets) {
-  for (let i = 0; i < tweets.length; i += 1){
-    const tweet = createTweetElement(tweets[i]);
-    tweetContainer.prepend(tweet);
-  };
-}
+        </article>
+    `;
+    return($tweet);  
+  }
 
 
-function loadTweets() {
-  $.ajax({
-    method: "GET",
-    url: "/tweets",
-    dataType: 'json'
-  }).done(function(data) {
-      $('#all-tweets').empty();
-      renderTweets(data);     
-  });
-}
+  let tweetContainer = $('#all-tweets');
 
-loadTweets();
+  function renderTweets(tweets) {
+    for (let i = 0; i < tweets.length; i += 1){
+      const tweet = createTweetElement(tweets[i]);
+      tweetContainer.prepend(tweet);
+    };
+  }
 
-// post request from user
-$("#tweets-maker").on('submit', function(event) {
-  // prevent the default behavor
-  event.preventDefault();
-  
-  // get the data from the form
-  // ajax post request
-  const serialized = $(this).serialize();
-  let form = this;
 
-  $.ajax({    
-    method: "POST",
-    url: "/tweets",
-    data: serialized
-  }).done(function() {    
-    form.reset();   // cleans the form area
-    $("#counter").text("140");    // sets the label for the original max
+  function loadTweets() {
+    $.ajax({
+      method: "GET",
+      url: "/tweets",
+      dataType: 'json'
+    }).done(function(data) {
+        $('#all-tweets').empty();
+        renderTweets(data);     
+    });
+  }
 
-    // on success, refresh the creaks on the page    
-    loadTweets(serialized);
+  loadTweets();
+
+  // post request from user
+  $("#tweets-maker").on('submit', function(event) {
+    const counter = $('#counter');
+    console.log("counter.text(): ", counter.text());
+    if (((counter.text()) >= 0) && (counter.text() < 140)){
+      alert("OK");
+
+      // prevent the default behaviour
+      event.preventDefault();
+      
+      // get the data from the form
+      // ajax post request
+      const serialized = $(this).serialize();
+      let form = this;
+
+      $.ajax({    
+        method: "POST",
+        url: "/tweets",
+        data: serialized
+      }).done(function() {    
+        form.reset();   // cleans the form area
+        $("#counter").text("140");    // sets the label for the original max
+
+        // on success, refresh the creaks on the page    
+        loadTweets(serialized);
+      });
+    } else {
+      alert("Please the message is suppose to be bigger than 0 and up to 140 characters.");
+
+      // prevent the default behaviour
+      event.preventDefault();
+    }
   });
 });
+
+// https://formden.com/blog/validate-contact-form-jquery
