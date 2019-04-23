56b8984da9c536ccbebf181db32d8e8bebf3f163
Tony Kieling <tony.kieling@gmail.com>
2019-04-22 21:05:54 -0700

    changes in the readme and tweets_example

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ This repository is the starter code for the project: Students will fork and clon
 
 1. Fork this repository, then clone your fork of this repository.
 2. Install dependencies using the `npm install` command.
-3. Start the web server using the `node server/index.js` command.
+3. Start the web server using the `npm start` or `node server/index.js` command.
 4. Go to <http://localhost:8080/> in your browser.
 5. For development and test purposes, there is a file in 'database/tweets_example.js' which cleans and starts the database with 3 documents. Just in case, run `node database/tweets_example.js`
 
---
+++ database/tweets_example.js
@@ -0,0 +1,80 @@
+"use strict";
+
+const { MongoClient } = require("mongodb");
+const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+
+MongoClient.connect(MONGODB_URI, (err, db) => {
+  if (err) {
+    console.error(`Failed to connect: ${MONGODB_URI}`);
+    throw err;
+  }
+
+  // We have a connection to the "tweeter" db, starting here.
+  console.log(`Connected to Mongodb: ${MONGODB_URI}`);
+
+  // remove all document from 
+  console.log("checking and cleaning the documents..");
+
+  const documents = db.collection("tweets").find();
+  documents ?
+    db.collection("tweets").remove({}) :
+    null;
+
+  const tweets = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": {
+          "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
+          "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
+          "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
+        },
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1508630651870
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": {
+          "small":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_50.png",
+          "regular": "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc.png",
+          "large":   "https://vanillicon.com/7b89b0d8280b93e2ba68841436c0bebc_200.png"
+        },
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1546630651870
+    },
+    {
+      "user": {
+        "name": "Johann von Goethe_",
+        "avatars": {
+          "small":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_50.png",
+          "regular": "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1.png",
+          "large":   "https://vanillicon.com/d55cf8e18b47d4baaf60c006a0de39e1_200.png"
+        },
+        "handle": "@johann49"
+      },
+      "content": {
+        "text": "Es ist nichts schrecklicher als eine tätige Unwissenheit."
+      },
+      "created_at": 1461113796368
+    }
+  ];
+
+
+  // insert the 3 above
+  console.log("inserting 3 new tweets...");
+  db.collection("tweets").insert(tweets);
+
+  console.log('Done!!')
+  console.log("3 documents have been inserted to the Mongodb as tweets' examples.");
+
+  db.close();
+
+});
